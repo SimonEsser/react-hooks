e2ca295acf8eeb6989701297a83da1bca816d38e
s.esser <simon.esser@datamediq.com>
2022-05-13 16:19:08 +0200

    enables tests for exercise 1

--- src/__tests__/01.js
+++ src/__tests__/01.js
@@ -1,11 +1,12 @@
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import App from '../final/01'
-// import App from '../exercise/01'
+// import App from '../final/01'
+import App from '../exercise/01'
 
 test('typing a name shows a greeting', async () => {
   render(<App />)
+  expect(screen.getByText(/hello.*simon/i)).toBeInTheDocument()
   await userEvent.type(screen.getByRole('textbox', {name: /name/i}), 'bob')
   expect(screen.getByText(/hello.*bob/i)).toBeInTheDocument()
 })
