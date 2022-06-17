4e0d744ed50e30b16797ae78ec0fac48f64c4ef7
s.esser <simon.esser@datamediq.com>
2022-06-17 15:27:46 +0200

    completes extra credit 1 for exercise 04

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -5,7 +5,10 @@ import * as React from 'react'
 
 function Board() {
   // 🐨 squares is the state for this component. Add useState for squares
-  const [squares, setSquares] = React.useState(Array(9).fill(null))
+  const [squares, setSquares] = React.useState(
+    () =>
+      JSON.parse(window.localStorage.getItem('squares')) ?? Array(9).fill(null),
+  )
 
   // 🐨 We'll need the following bits of derived state:
   // - nextValue ('X' or 'O')
@@ -17,27 +20,31 @@ function Board() {
   const winner = calculateWinner(squares)
   const status = calculateStatus(winner, squares, nextValue)
 
+  React.useEffect(() => {
+    window.localStorage.setItem('squares', JSON.stringify(squares))
+  }, [squares])
+
   // This is the function your square click handler will call. `square` should
   // be an index. So if they click the center square, this will be `4`.
   function selectSquare(squareInd) {
     // 🐨 first, if there's already winner or there's already a value at the
     // given square index (like someone clicked a square that's already been
     // clicked), then return early so we don't make any state changes
-    if (winner || squares[squareInd]){
+    if (winner || squares[squareInd]) {
       return
     }
-    
+
     // 🦉 It's typically a bad idea to mutate or directly change state in React.
     // Doing so can lead to subtle bugs that can easily slip into production.
     //
     // 🐨 make a copy of the squares array
     // 💰 `[...squares]` will do it!)
     const squaresCopy = [...squares]
-    
+
     // 🐨 set the value of the square that was selected
     // 💰 `squaresCopy[square] = nextValue`
     squaresCopy[squareInd] = nextValue
-    
+
     // 🐨 set the squares to your copy
     setSquares(squaresCopy)
   }
